37b9e6d91501302b4ed64e3d695fd91acadfacc1
Topher Sikorra <iareintelligent@gmail.com>
2017-12-20 20:14:58 -0500

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 // import { FavoriteChangedEventArgs } from "./favorite/favorite.component";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [];
+  viewMode = 'map';
 }
